7bd320ba23d5f438a8183a3c79e8d939a432ab68
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-01-31 07:55:51 +0400

    Demo

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
-//index
-document.getElementById("easy").addEventListener("click", function() {
-  window.location.href = "easy.html";
-});
-document.getElementById("hardBtn").addEventListener("click", function() {
- 
-  window.location.href = "hard.html";
-});
 
-//easy
-const maincolor = document.querySelector(".Main")
-const startBtn = document.getElementById("startBtn");
 
-function randomColor() {
-  const r = Math.floor(Math.random() * 256); // 0-255
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  return `rgb(${r}, ${g}, ${b})`;
+let score = 0;
+let timer;
+
+// const levelTime = 1500; 
+const colors = ["red",, "green", "blue"];
+
+function startGame() {
+  clearTimeout(timer);
+
+  const colorIndex = Math.floor(Math.random() * colors.length);
+  const targetColor = colors[colorIndex];
+
+  const colorBigBox = document.querySelector(".Main");
+  colorBigBox.style.backgroundColor = targetColor;
+
+  const scoreDisplay = document.getElementById("score");
+  const messageDisplay = document.createElement("h2");
+  document.querySelector(".down").appendChild(messageDisplay);
+
+  const answerButtons = document.querySelectorAll(".Answers span");
+  answerButtons.forEach(function (button) {
+    button.onclick = function () {
+      
+      const chosenColor = this.style.backgroundColor;
+      if (chosenColor === targetColor) {
+        score++;
+      } else {
+        score--;
+      }
+      scoreDisplay.textContent = "score : " + score;
+
+     
+      if (score >= 4) {
+        messageDisplay.textContent = "გილოცავ, მოიგე!";
+      } else if (score <= -4) {
+        messageDisplay.textContent = "სამწუხაროდ დამარცხდი, სცადე თავიდან";
+        score = 0;
+        scoreDisplay.textContent = "score : " + score;
+      } else {
+        startGame(); 
+      }
+    };
+  });
+
+  
+  timer = setTimeout(function () {
+    score = 0;
+    
+  }, levelTime);
 }
 
-// მაგალითი: 5 შემთხვევითი ფერის მასივი
-function generateColors(num) {
-  const arr = [];
-  for (let i = 0; i < num; i++) {
-    arr.push(randomColor());
-  }
-  return arr;
-}
-
-
-
-
+// Start ღილაკი
+document.getElementById("startBtn").addEventListener("click", function () {
+  score = 0;
+  document.getElementById("score").textContent = "score : 0";
+  startGame();
+});
